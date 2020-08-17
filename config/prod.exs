b2af081f074de3f7314e2f1d862b30052c2967f4
Anup Dhakal <20139982+exanup@users.noreply.github.com>
2020-08-18 01:09:18 +0545

    Enable compression

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :blog_phoenix, BlogPhoenixWeb.Endpoint,
-  http: [port: {:system, "PORT"}],
+  http: [port: {:system, "PORT"}, compress: true],
   url: [host: "anup-blogs.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
